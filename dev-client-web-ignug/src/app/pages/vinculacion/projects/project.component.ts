188cc5a583fc758c7986f902c03d26f6a92f2f0d
DanielaTobar <nadi_toci@outlook.com>
2020-07-29 14:05:34 -0500

    cambio de rol listo, redireccionamiento a formulario de docente listo

--- dev-client-web-ignug/src/app/pages/vinculacion/projects/project.component.ts
+++ dev-client-web-ignug/src/app/pages/vinculacion/projects/project.component.ts
@@ -1,12 +1,25 @@
-import {Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CarService } from '../../../demo/service/carservice';
+import { Car } from '../../../demo/domain/car';
 
 @Component({
     selector: 'app-vinculacion-project',
-    templateUrl: './project.component.html'
+    templateUrl: './project.component.html',
 })
 export class ProjectsComponent implements OnInit {
 
-    ngOnInit() {
+    cars: Car[];
 
+    carsLarge: Car[];
+
+    //rol = 'coordinador';
+    rol = 'docente';
+
+
+    constructor(private carService: CarService,) {
+    }
+
+    ngOnInit() {
+        this.carService.getCarsLarge().then(cars => this.carsLarge = cars);
     }
 }
--- dev-client-web-ignug/src/app/pages/vinculacion/vinculacion.module.ts
+++ dev-client-web-ignug/src/app/pages/vinculacion/vinculacion.module.ts
@@ -4,12 +4,21 @@ import {CommonModule} from '@angular/common';
 import {ProjectsComponent} from './projects/project.component';
 import {FormsComponent} from './forms/form.component'
 import {HomeRoutes} from './vinculacion.routing';
+import {DataViewModule} from 'primeng/dataview';
+import {PanelModule} from 'primeng/panel';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
+
 import { from } from 'rxjs';
 
 @NgModule({
     imports: [
         CommonModule,
         RouterModule.forChild(HomeRoutes),
+        DataViewModule,
+        PanelModule,
+        InputTextModule,
+        ButtonModule,
     ],
     declarations: [
         ProjectsComponent,
