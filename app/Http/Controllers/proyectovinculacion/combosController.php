ddd93b2b8d084d489d07f4958a56323b2b250a5c
elpolituber <franco-programador@hotmail.com>
2020-08-14 11:03:23 -0500

    Acabado de la mision de los combos
    se termino  el la creacion de los desplegables (combos) de para el uso
    del api para que se oueda ejecutar dentro dtro de lois terminos del uso
    de api

--- app/Http/Controllers/proyectovinculacion/combosController.php
+++ app/Http/Controllers/proyectovinculacion/combosController.php
@@ -8,6 +8,11 @@ use Illuminate\Http\Request;
 use App\Models\Vinculacion\AcademiPeriod;
 use App\Models\Vinculacion\Career;
 use App\Models\Vinculacion\FraquencyOfActivity;
+use App\Models\Vinculacion\LinkageAxes;
+use App\Models\Vinculacion\AssignedLine;
+use App\Models\Vinculacion\BondingActivities;
+use App\Models\Vinculacion\MeansOfVerification;
+use App\Models\Vinculacion\Mode;
 
 
 class combosController extends Controller
@@ -15,8 +20,22 @@ class combosController extends Controller
   public function show(){
     $academiPreriod=AcademiPeriod::all("details","year");
     $career=Career::all("description");
-    $fraquencyOfActivity=FraquencyOfActivity::all();
-    $combos=array("academiPreriod"=>$academiPreriod,"career"=>$career);
+    $fraquencyOfActivity=FraquencyOfActivity::all("details");
+    $LinkageAxes=LinkageAxes::all("details");
+    $AssignedLine=AssignedLine::all("details");
+    $BondingActivities=BondingActivities::all("details");
+    $MeansOfVerification=MeansOfVerification::all("details");
+    $Mode=Mode::all('name');
+    $combos=array(
+        "academiPreriod"=>$academiPreriod,
+        "career"=>$career,
+        "fraquencyOfActivity"=>$fraquencyOfActivity,
+        "LinkageAxes"=>$LinkageAxes,
+        "AssignedLine"=>$AssignedLine,
+        "BondingActivities"=>$BondingActivities,
+        "MeansOfVerification"=>$MeansOfVerification,
+        "Mode"=>$Mode
+      );
     return $combos;
  }
 
